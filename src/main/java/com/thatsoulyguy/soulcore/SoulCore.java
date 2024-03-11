999ce864a32ea2b72db182b2b6be79e84fffebd1
ThatSoulyGuy <bkmcman10bk@gmail.com>
2024-03-11 11:42:23 -0500

    Localized Update System

--- src/main/java/com/thatsoulyguy/soulcore/SoulCore.java
+++ src/main/java/com/thatsoulyguy/soulcore/SoulCore.java
@@ -1,23 +1,13 @@
 package com.thatsoulyguy.soulcore;
 
-import com.thatsoulyguy.soulcore.update.SUpdateManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public final class SoulCore extends JavaPlugin
 {
     @Override
     public void onEnable()
     {
-        new BukkitRunnable()
-        {
-            @Override
-            public void run()
-            {
-                for(Runnable call : SUpdateManager.GetUpdateCalls())
-                    call.run();
-            }
-        }.runTaskTimer(this, 0L, 20L);
+
     }
 
     @Override
--- src/main/java/com/thatsoulyguy/soulcore/update/SUpdateManager.java
+++ src/main/java/com/thatsoulyguy/soulcore/update/SUpdateManager.java
@@ -1,5 +1,8 @@
 package com.thatsoulyguy.soulcore.update;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +10,19 @@ import java.util.Map;
 public class SUpdateManager
 {
     private static final Map<String, Runnable> updateCalls = new HashMap<>();
+
+    public static void Initialize(JavaPlugin plugin)
+    {
+        new BukkitRunnable()
+        {
+            @Override
+            public void run()
+            {
+                for(Runnable call : SUpdateManager.GetUpdateCalls())
+                    call.run();
+            }
+        }.runTaskTimer(plugin, 0L, 20L);
+    }
 
     public static void Register(String id, Runnable function)
     {
